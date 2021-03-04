842ecc61307f223324be07f5e80d356cb1b1a1f7
Lucas Axelson <lucasaxelson2016@GMAIL.COM>
2021-03-04 10:45:17 +0000

    Cookie Message - 1/2

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault()
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -29,3 +30,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Creating Cookie Message 
+const message = document.createElement(`div`)
+message.classList.add(`cookie`)
+message.innerHTML = 'We use your cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+
+header.prepend(message)
+header.after(message)
+
+// Removing Cookie Message
+document.querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    message.remove()
+  })
+
+// Cookie Message - Styles
+message.style.backgroundColor = `#37383d`
+message.style.width = `120%`
+message.style.height = Number.parseFloat(message.getComputedStyle(message).height, 10) + 40 + `px`
+
+// Alters all buttons over to an Orange Red --Example
+// document.documentElement.style.setProperty(`--color-primary`, `orangered`) 
+
+const logo = document.querySelector(`nav__logo`)
+
+// Data attributes
+
+const logo = document.querySelector(`nav__logo`)
