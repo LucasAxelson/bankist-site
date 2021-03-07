c8242a3f6c53e6310763076c37077eff3811cb81
Lucas Axelson <lucasaxelson2016@GMAIL.COM>
2021-03-07 13:25:55 +0000

    Tab transition

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) +
 // document.documentElement.style.setProperty(`--color-primary`, `orangered`) 
 
 ///////////////////////////////////////
-// Scrolling
+// Page Navigation
 
 const btnScrollTo = document.querySelector(`.btn--scroll-to`)
 const section1 = document.querySelector(`#section--1`)
@@ -69,8 +69,7 @@ btnScrollTo.addEventListener(`click`, function (e) {
   section1.scrollIntoView({ behavior: `smooth` })
 })
 
-///////////////////////////////////////
-// Page Navigation
+// Link Navigation ===============================
 
 document.querySelector(`.nav__links`).addEventListener
   (`click`, function (e) {
@@ -84,14 +83,23 @@ document.querySelector(`.nav__links`).addEventListener
     }
   })
 
+// Tabbed components
 
-// document.querySelectorAll(`.nav__link`).forEach(function (el) {
-//   el.addEventListener(`click`, function (e) {
-//     e.preventDefault()
-//     const id = this.getAttribute(`href`)
-//     console.log(id)
-//     document.querySelector(id).scrollIntoView({
-//       behavior: 'smooth'
-//     })
-//   })
-// })
+const tabs = document.querySelectorAll(`.operations__tab`)
+const tabsContainer = document.querySelectorAll(`.operations__tab-container`)
+const tabsContent = document.querySelectorAll(`.operations__content`)
+
+tabsContainer.addEventListener(`click`, (e) => {
+  const clicked = e.target.closest(`.operations__tab`)
+  if (!clicked) return // Guard clause
+
+  // Remove Tab
+  tabsContainer.forEach(t => t.classList.remove(`.operations__tab--active`))
+  tabsContent.forEach(c => c.classList.remove(`operations__contant--active`))
+
+  // Activate Tab
+  clicked.classList.add(`.operations__tab--active`)
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add(`.operations__content--active`)
+})
