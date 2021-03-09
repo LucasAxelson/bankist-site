707e8e66a8f3ce29d53615405a731d7b6350d2c3
Lucas Axelson <lucasaxelson2016@GMAIL.COM>
2021-03-09 07:09:30 +0000

    Sticky Nav

--- script.js
+++ script.js
@@ -5,13 +5,13 @@
 
 const header = document.querySelector(`.header`)
 const nav = document.querySelector(`.nav`)
+const tabs = document.querySelectorAll(`.operations__tab`)
+const tabsContainer = document.querySelector(`.operations__tab-container`)
+const tabsContent = document.querySelectorAll(`.operations__content`)
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const tabs = document.querySelectorAll(`.operations__tab`)
-const tabsContainer = document.querySelectorAll(`.operations__tab-container`)
-const tabsContent = document.querySelectorAll(`.operations__content`)
 
 const openModal = function (e) {
   e.preventDefault()
@@ -92,7 +92,7 @@ tabsContainer.addEventListener(`click`, (e) => {
   const clicked = e.target.closest(`.operations__tab`)
   if (!clicked) return // Guard clause
 
-  // Remove Tab
+  // Remove Tab 
   tabsContainer.forEach(t => t.classList.remove(`.operations__tab--active`))
   tabsContent.forEach(c => c.classList.remove(`operations__contant--active`))
 
@@ -102,3 +102,48 @@ tabsContainer.addEventListener(`click`, (e) => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add(`.operations__content--active`)
 })
+
+// Menu Fade Animation ===============================
+
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`)
+    const logo = link.closest(`.nav`).querySelector(`img`)
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity
+    })
+    logo.style.opacity = opacity
+  }
+}
+
+nav.addEventListener(`mouseover`, function (e) {
+  handleHover(e, 0.5)
+})
+
+nav.addEventListener(`mouseout`, function (e) {
+  handleHover(e, 1)
+})
+
+// Sticky Navigation ===============================
+
+const header = document.querySelector(`.header`)
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = (entries) {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) {
+    nav.classList.add(`sticky`)
+  } else nav.classList.remove(`sticky`)
+}
+
+const headerObserver = new IntersectionObserver
+  (stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+  })
+
+headerObserver.observe(header)
